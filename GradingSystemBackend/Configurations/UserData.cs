01629015c1f668f177bd1e92a07fb981ed078d90
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2024-01-27 15:25:48 +0800

    Annotate User Data

--- GradingSystemBackend/Configurations/UserData.cs
+++ GradingSystemBackend/Configurations/UserData.cs
@@ -1,10 +1,19 @@
-﻿namespace GradingSystemBackend.Configurations
+﻿using Newtonsoft.Json;
+
+namespace GradingSystemBackend.Configurations
 {
     public class UserData
     {
+        [JsonProperty("email")]
         public string Email { get; set; }
+
+        [JsonProperty("userName")]
         public string UserName { get; set; }
+
+        [JsonProperty("firstName")]
         public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
         public string LastName { get; set; }
     }
 }
